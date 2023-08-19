e65428c503bfa7ca5346bac5d392b8ecc118c6a5
sgonzalez <sgonzalezoyuela@gmail.com>
2023-08-19 00:44:08 -0300

    java8 flake

---
+++ java8/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based Java 8 + Maven dev env";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      javaVersion = 8;
+      overlays = [
+        (final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          maven = prev.maven.override { inherit jdk; };
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ jdk maven ];
+        };
+      });
+    };
+}
